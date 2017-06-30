07e88f6e2fc77d76a284846b4f264b9caf02a3eb
Cherisher <cool0616@163.com>
2017-06-30 16:17:35 +0800

    Update 2345.py

--- 2345.py
+++ 2345.py
@@ -1,253 +1,196 @@
-#!/bin/env python
+#!/usr/bin/python
 # -*- coding: utf8 -*-
 """
+
 Auth: Candoit
+
 Date: 2016-04-05
+
 """
 import urllib
 import urllib2
 import cookielib
 import time
 import random
+import json
 import hashlib
+import sys
 
+Encoding='gzip, deflate'
+loginCookie=''
+phpsessid=''
+uid='24311802'
 
-class HttpRedirect_Handler(urllib2.HTTPRedirectHandler):
-    def http_error_302(self, req, fp, code, msg, headers):
-        return urllib2.HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, headers)
-
-def checkNeedVirCode(url, username):
-    data="cmd=isNeedCaptcha&showWay=ajax&username=" + username
-
-    send_headers = {
-        'Host': 'jifen.2345.com',
-        'Connection': 'keep-alive',
-        'Content-Length': len(data),
-        'Accept': '*/*',
-        'Origin': 'http://jifen.2345.com',
-        'X-Requested-With': 'XMLHttpRequest',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'DNT': '1',
-        'Referer': 'http://jifen.2345.com/',
-        'Accept-Encoding': 'xzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4,zh-TW;q=0.2'
-         }
-    req = urllib2.Request(url, headers=send_headers)
-
-    req.add_data(data)
-    response = urllib2.urlopen(req).read()
-    print response
-    if int(response) == 0:
-        print "noNeedCaptcha"
-        return True
-    else:
-        print "NeedCaptcha"
-        return False
-
-def checkTime(url):
-    currtime = int(time.time())
-    data = "cmd=vdate&currtime=" + str(currtime)
-    send_headers = {
-        'Host': 'jifen.2345.com',
-        'Connection': 'keep-alive',
-        'Content-Length': len(data),
-        'Accept': '*/*',
-        'Origin': 'http://jifen.2345.com',
-        'X-Requested-With': 'XMLHttpRequest',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'DNT': '1',
-        'Referer': 'http://jifen.2345.com/',
-        'Accept-Encoding': 'xzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4,zh-TW;q=0.2'
-     }
-    req = urllib2.Request(url, headers=send_headers)
-
-    req.add_data(data)
-    response = urllib2.urlopen(req).read()
-    if response == 'time_err':
-        print 'time error'
-        return False
-    else:
-        print 'time success'
-        return True
-
-
-def doSignature(opener, hostname):
-    url = 'http://'+hostname+'/jifen/every_day_signature_new.php'
-    data = "sign_token="
-
-    currtime = time.time()
-    midnight = currtime - (currtime % 86400) + time.timezone # 当天零点的时间
-    code = str(int(midnight)) + uid[::-1] # uid 反转
-
-    """
-    code 算法：
-    当天零点的时间 + uid字符串反转
-    """
-    m2 = hashlib.md5()
-    m2.update(uid+ code)
-    data += m2.hexdigest()
-
-    req = urllib2.Request(url)
-    tmp = ""
-    for item in cookie:
-        tmp += item.name+ "=" + item.value+"; "
-
-    print 'Cookie:',tmp
-    req.add_header('Cookie', tmp)
-    req.add_header('Connection', 'keep-alive')
-    req.add_header('Accept-Encoding', 'xzip, deflate, sdch')
-    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36')
-    req.add_header('Accept-Language', 'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4,zh-TW;q=0.2')
-    req.add_header('DNT', '1')
-    req.add_header('Referer', 'http://'+hostname+'/index.php')
-    req.add_header('Origin', 'http://'+hostname)
-    req.add_header('Content-Type','application/x-www-form-urlencoded; charset=UTF-8')
-    req.add_header('X-Requested-With', 'XMLHttpRequest')
-    req.add_header('Accept', '*/*')
-    req.add_header('Content-Length', len(data))
-
-    req.add_data(data)
-    print 'doSignature'
-    response = urllib2.urlopen(req);
-
-    print response.read()
-
-
-def set2345Cookie(url_str):
-    """
-        HTTP/1.1 302 Found
-        Date: Thu, 07 Apr 2016 02:24:54 GMT
-        Server: Apache
-        P3P: CP='NOI ADM DEV PSAi COM NAV OUR OTRo STP IND DEM'
-        Set-Cookie: I=i%3D23741766%26u%3D24311802%26n%3D%25B0%25B5%25D3%25B0%25D6%25AE%25D3%25E3%26t%3D1459995894.51632200%26s%3D71ffe08c34ccef8afca5655a8f8b091b%26v%3D1.0%26ismobile%3D0; expires=Thu, 07-Apr-2016 04:24:54 GMT; path=/; domain=.2345.cn; httponly
-        Set-Cookie: uid=24311802; expires=Thu, 07-Apr-2016 04:24:54 GMT; path=/; domain=.2345.cn
-        Location: http://jifen.2345.com
-        Cache-Control: max-age=0
-        Expires: Thu, 07 Apr 2016 02:24:54 GMT
-        Vary: Accept-Encoding
-        Content-Length: 0
-        Connection: close
-        Content-Type: text/html; charset=gbk
-    """
-    send_headers = {
-        'Connection': 'keep-alive',
-        'Accept-Encoding': 'xzip, deflate, sdch',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36',
-        'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4,zh-TW;q=0.2',
-        'Accept': 'image/webp,image/*,*/*;q=0.8',
-        'DNT': '1',
-        'Referer': 'http://jifen.2345.com/'}
-
-    req = urllib2.Request(url_str, headers=send_headers)
-
-    opener = urllib2.build_opener(HttpRedirect_Handler)
-
-    response = opener.open(req)
-
-    data = response.read()
-
-    doSignature(opener, 'jifen.2345.com')
-    doSignature(opener, 'shouji.2345.com')
-
-def checkLoginResponse(response):
-    errmsg ={1: '验证码输入错误，请重新输入',
-           4:  '2345帐号不存在或密码错误!',
-           7:'请输入验证码 :)',
-           8:'登录有误，错误代码1004，请联系客服 :)',
-           9:'登陆异常, 请稍后再试 :)',
-           10:'2345帐号异常'}
-
-    redata = response.strip("[]").split(',')[0:4]
-    op = int(redata[0])
-    if op == 6:
-        url_str = redata[1].replace('\/', '/').strip("\"")
-
-        if url_str.find('bbs.2345.cn/api/passport.php?action=login&cookieTime') >= 0:
-            print 'setCookie'
-            set2345Cookie(url_str)
-        else:
-            print url_str
-    elif op == 2 or op == 3 or op == 5:
-        url_str = redata[1].replace('\/', '/').strip("\"")
-        print url_str
-
-    else:
-        print errmsg[op]
-
-def doLogin(forward, url, username, password):
-    vTime ="7776000"
-    weakpass="0"
-    remember = '0'
-    imgCodeStr=''
-
-    """
-    ·# 验证码处理
-     if(checkCookie('lnc') == 1) {
-     	   imgCodeStr = "&pImgCode=" + $("#pImgCode").val() + "";
-        }
-    """
-    global cookie
+def DoLogin():
+    global loginCookie
+    global phpsessid
     global uid
+    
+    url = 'https://passport.2345.com/clientapi/login.php'
+    #password md5
+    data='captcha_code=&password=11111111111111111111111111111111111&username=111111111111111111111'
 
     cookie = cookielib.CookieJar()
     handler=urllib2.HTTPCookieProcessor(cookie)
 
-    m2 = hashlib.md5()
-    m2.update(password)
-    md5passwd = m2.hexdigest()
 
-    data = "cmd=login&username=" + username + "&password=" + md5passwd + "&remember=" + remember + "&forward=" + forward + "&showWay=ajax"+imgCodeStr+"&weakpass="+weakpass+"&t=" + str(random.random())
-    send_headers = {
-        'Host': 'jifen.2345.com',
-        'Connection': 'keep-alive',
-        'Content-Length': len(data),
-        'Accept': '*/*',
-        'Origin': 'http://jifen.2345.com',
-        'X-Requested-With': 'XMLHttpRequest',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'DNT': '1',
-        'Referer': 'http://jifen.2345.com/',
-        'Accept-Encoding': 'xzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4,zh-TW;q=0.2'
-     }
-    req = urllib2.Request(url, headers=send_headers)
+    req = urllib2.Request(url)
+    req.add_header('Host', 'passport.2345.com')
+    req.add_header('Accept', '*/*')
+    req.add_header('Accept-Encoding', 'xzip, default')
+    req.add_header('Content-Length', len(data))
+    req.add_header('Content-Type','application/x-www-form-urlencoded')
+    req.add_header('Accept-Language', 'zh-Hans;q=1, en-US;q=0.9')
+    req.add_header('Cookie', 'name_ie=%2526263%2524433%2520043%2540060; uid='+uid)
+    req.add_header('Connection', 'keep-alive')
+    req.add_header('User-Agent', 'union/1.0 (iPhone; iOS 9.3.2; Scale/2.00)')
     req.add_data(data)
 
     opener = urllib2.build_opener(handler)
 
     response = opener.open(req).read()
-
-
     #print cookie
+
+
     for item in cookie:
         print 'Name = '+item.name
         print 'Value = '+item.value
         if item.name == 'uid':
             uid = item.value
+        elif item.name == 'PHPSESSID':
+            phpsessid = item.value
+    s = json.loads(response)
+    loginCookie =  s['data']['I']
 
-    print "uid:", uid
+def AppIOSv3():
+    global loginCookie
+    global phpsessid
+    global uid
+    global Encoding
+    #url = 'http://jifen.2345.com/appiosv2/sign/index'
+    url = 'http://jifen.2345.com/appv3/index/index?channel=1&ioschannel=appstore&iosversion=2.0'
+    cookiestr = 'I='+ loginCookie +'; os=iosnew; requestFromApp=1; appversionuid=300' #+uid + ';PHPSESSID=' + phpsessid
 
-    checkLoginResponse(response)
+    print cookiestr
+    
+    cookie = cookielib.CookieJar()
+    handler=urllib2.HTTPCookieProcessor(cookie)
 
 
-if __name__ == '__main__':
-    forward = 'http://jifen.2345.com'
-    url = "/jifenLogin.php"
-    url =forward + url
+    req = urllib2.Request(url)
+    req.add_header('Host', 'jifen.2345.com')
+    req.add_header('Accept', '*/*')
+    req.add_header('Accept-Encoding', Encoding)    
+    req.add_header('Cookie',cookiestr)
+    req.add_header('token', '660ce50559c21986422a8e36a334e671')
+    req.add_header('Connection', 'keep-alive')
+    req.add_header('User-Agent', 'unionPersonal/2.0 (iPhone; iOS 9.3.3; Scale/2.00)')    
 
-    username = "" # 用户名
-    password = "" # 密码
+    opener = urllib2.build_opener(handler)
+    
+    response = opener.open(req).read()
+    #print response
 
-    if len(username) == 0 or len(password) == 0:
-        print "!!!Please input you username and password!!!"
-        sys.exit(1)
+def doSign(host, req):
+    global loginCookie
+    global phpsessid
+    global uid
+    global Encoding
+    url = 'http://'+host+'/jifen/every_day_signature_new.php'
+    
+    
+    cookiestr = 'I='+ loginCookie +'; PHPSESSID=' + phpsessid +'; appversion=300; os=iosnew; path=/; requestFromApp=1; uid='+uid+'; name_ie=%2526263%2524433%2520043%2540060; uid='+uid
+    POST_data = "?sign_token="
+    if host == 'jifen.2345.com' or host == 'shouji.2345.com':
+         currtime = time.time()
+         midnight = currtime - (currtime % 86400) + time.timezone # 当天零点的时间
+         code = str(int(midnight)) + uid[::-1] # uid 反转
+         """
+code 算法：
+当天零点的时间 + uid字符串反转
+"""
+         m2 = hashlib.md5()
+         m2.update(uid+ code)
+         POST_data += m2.hexdigest()
+    
+    POST_data += "&channel=1"    
 
-    if checkNeedVirCode(url, username) == True and checkTime(url) == True:
-        print 'Do Login'
-        doLogin(forward, url, username, password)
-    else:
-        sys.exit(1)
+    req = urllib2.Request(url+POST_data)    
+    req.add_header('Host', host)    
+    req.add_header('Accept-Language', 'zh-cn')
+    req.add_header('User-Agent', 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_3 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13G34')
+    req.add_header('X-Requested-With', 'XMLHttpRequest')
+    req.add_header('Accept', '*/*')
+    req.add_header('Referer', 'http://'+host+'/appv3/sign/index')
+    req.add_header('Content-Type','application/x-www-form-urlencoded; charset=UTF-8')
+    req.add_header('Connection', 'keep-alive')
+    req.add_header('Cookie', cookiestr)
+    req.add_header('Origin', 'http://'+ host)
+    #req.add_header('Content-Length', len(POST_data))
+    req.add_header('Accept-Encoding', Encoding)
+    
+    #req.add_data(POST_data)
+    
+    print 'doSignature'
+    response = urllib2.urlopen(req);
+
+    print response.read()
+    
+def doSignV3(host):
+    global loginCookie
+    global phpsessid
+    global uid
+    global Encoding
+
+    url = 'http://'+ host +'/appv3/sign/index'
+
+    cookiestr = 'I='+ loginCookie +';os=iosnew;requestFromApp=1;appversion=300;uid='+ uid +';path=/;PHPSESSID=;'+phpsessid
+    
+    req = urllib2.Request(url)
+
+    req.add_header('Host', host)
+    req.add_header('Accept-Encoding', Encoding)
+    req.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
+    req.add_header('Cookie', cookiestr)
+    req.add_header('token', '660ce50559c21986422a8e36a334e671')
+    req.add_header('Accept-Language', 'zh-cn')
+    req.add_header('Connection', 'keep-alive')
+    req.add_header('User-Agent', 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_3 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13G34')
+
+    print 'doSignV3'
+    response = urllib2.urlopen(req);
+    #print response.read()
+
+    doSign(host, req)
+
+    
+def CheckAPPVersion():
+    global Encoding
+    url = 'http://jifen.2345.com/appv3/api/bankaddress?v=20160215'
+    req = urllib2.Request(url)
+    
+    req.add_header('Host', 'jifen.2345.com')    
+    req.add_header('Connection', 'Keep-Alive')    
+    req.add_header('Accept-Encoding', Encoding)    
+    
+    response = urllib2.urlopen(req);
+
+    data = json.loads(response.read())
+    if data['status'] == '200':
+        return 1
+   
+    return 0 
+
+
+if not CheckAPPVersion():
+    print "Error version"
+    sys.exit(0)
+    
+print 'Version is OK'
+DoLogin()
+AppIOSv3()
+
+#doSignV3('shouji.2345.com')
+doSignV3('jifen.2345.com')
+
+sys.exit(0)
